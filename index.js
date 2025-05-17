2e97d09cbd9d9b703b9e5a29eea759f604839cc0
Loay_Saifan <loaysaifan@gmail.com>
2025-05-17 10:09:37 -0300

    added a delete textbox button

--- index.js
+++ index.js
@@ -2,12 +2,25 @@ const button = document.getElementById("btn");
 const app = document.getElementById("layer1")
 
 function createNote(content){
+    const dltBtn = document.createElement("div");
+    dltBtn.classList.add("note-container");
+
+
     const element = document.createElement("textarea");
     element.classList.add("note");
     element.placeholder = "Empty Note";
     element.value = content;
 
+    const btnBox = document.createElement("button");
+    btnBox.textContent = "delete";
+    btnBox.classList.add("deleteBtn");
+    btnBox.addEventListener("click", () => element.remove());
+    btnBox.addEventListener("click", () => btnBox.remove());
+
+
     app.appendChild(element);
+    element.appendChild(btnBox);
+    app.appendChild(btnBox);
 }
 
 button.addEventListener("click", () => createNote(""));
