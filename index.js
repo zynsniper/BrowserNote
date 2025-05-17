3598d471e9e52ea502c6effd99e523b6c7bcf504
Loay_Saifan <loaysaifan@gmail.com>
2025-05-17 12:21:04 -0300

    modified style.css, index.html, and index.js  slight tuning nothing major

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
 const button = document.getElementById("btn");
-const app = document.getElementById("layer1")
+const app = document.getElementById("layer1");
 
-function createNote(content){
+function createNote(content) {
+    const deleteBtn = document.createElement("button");
+    const noteContainer = document.createElement("div");
+    const textarea = document.createElement("textarea");
+
+    textarea.classList.add("note");
+    deleteBtn.classList.add("deleteBtn");
+    noteContainer.classList.add("note-container");
+
+    deleteBtn.textContent = "delete";
+
+    textarea.placeholder = "Empty Note";
+    textarea.value = content;
+
+    
+
+
+    deleteBtn.addEventListener("click", () => noteContainer.remove());
+
+    noteContainer.appendChild(deleteBtn);
+    app.appendChild(noteContainer);
+    noteContainer.appendChild(textarea);
+    
+}
+
+button.addEventListener("click", () => createNote(""));
+console.log("h");
+
+
+
+/*function createNote(content){
     const dltBtn = document.createElement("div");
     dltBtn.classList.add("note-container");
 
@@ -21,7 +51,4 @@ function createNote(content){
     app.appendChild(element);
     element.appendChild(btnBox);
     app.appendChild(btnBox);
-}
-
-button.addEventListener("click", () => createNote(""));
-console.log("h");
+}*/
