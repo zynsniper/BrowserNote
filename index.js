f10e753a2626a4d91d42b8bdc3b8cfec79035a92
zynsniper <cheang5337@gmail.com>
2025-05-17 02:32:46 -0300

    added createNote()

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const button = document.getElementById("btn");
+const app = document.getElementById("layer1")
+
+function createNote(content){
+    const element = document.createElement("textarea");
+    element.classList.add("note");
+    element.placeholder = "Empty Note";
+    element.value = content;
+
+    app.appendChild(element);
+}
+
+button.addEventListener("click", () => createNote(""));
+console.log("h");
